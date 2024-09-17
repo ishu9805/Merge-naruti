e4509c54ab91c3923014a270f1632ddafc486078
ishu9805 <abhishek47903@gmail.com>
2024-09-17 15:39:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, send_from_directory
 from motor.motor_asyncio import AsyncIOMotorClient
 import asyncio
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='frontend/static')
 
 # MongoDB connection URL
 mongo_url = "mongodb+srv://babusona:hinatababy@cluster0.t0lfelh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = AsyncIOMotorClient(mongo_url)
 db = client['Character_catcher']
 collection = db['anime_characters_lol']
+
+@app.route('/')
+def home():
+    return send_from_directory('frontend/static', 'index.html')
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('frontend/static', filename)
 
 @app.route('/waifus', methods=['GET'])
 async def get_waifus():
@@ -32,9 +40,6 @@ async def get_waifu(character_name):
         })
     else:
         return jsonify({'error': 'Waifu not found'}), 404
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
