3e4d466dedab2b00f06349b0f883d59b69b34b7c
ishu9805 <abhishek47903@gmail.com>
2024-09-17 17:29:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,38 +22,56 @@ def serve_static(filename):
 
 @app.route('/waifus/search', methods=['GET'])
 def search_waifus():
+    query = request.args.get('query', '')
+    regex_pattern = re.compile(f".*{re.escape(query)}.*", re.IGNORECASE)
+    waifus = list(collection.find({'name': regex_pattern}))
+    results = [{
+        'character_name': waifu['name'],
+        'anime_name': waifu['anime'],
+        'image_url': waifu['img_url'],
+        'rarity': waifu.get('rarity', 'Unknown'),
+        'id': waifu.get('id', 'N/A')
+    } for waifu in waifus]
+    return jsonify({'results': results})
+
+@app.route('/waifus', methods=['GET'])
+def get_characters():
     try:
-        query = request.args.get('query', '')
-      
-        regex_pattern = re.compile(f".*{re.escape(query)}.*", re.IGNORECASE)
-        waifus = list(collection.find({'name': regex_pattern}))
- 
+        page = int(request.args.get('page', 1))
+        size = int(request.args.get('size', 15))
+        skip = (page - 1) * size
+        limit = size
+
+        total_count = collection.count_documents({})
+        has_next_page = (total_count > page * size)
+
+        waifus = list(collection.find().skip(skip).limit(limit))
         results = [{
             'character_name': waifu['name'],
             'anime_name': waifu['anime'],
             'image_url': waifu['img_url'],
-            'rarity': waifu.get('rarity', 'Unknown')
+            'rarity': waifu.get('rarity', 'Unknown'),
+            'id': waifu.get('id', 'N/A')
         } for waifu in waifus]
-        return jsonify({'results': results})
+
+        return jsonify({'results': results, 'hasNextPage': has_next_page})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 @app.route('/waifus/<string:character_name>', methods=['GET'])
 def get_waifu(character_name):
-    try:
-        regex_pattern = re.compile(f".*{re.escape(character_name)}.*", re.IGNORECASE)
-        waifu = collection.find_one({'name': regex_pattern})
-        if waifu:
-            return jsonify({
-                'character_name': waifu['name'],
-                'anime_name': waifu['anime'],
-                'image_url': waifu['img_url'],
-                'rarity': waifu.get('rarity', 'Unknown')
-            })
-        else:
-            return jsonify({'error': 'Waifu not found'}), 404
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    regex_pattern = re.compile(f".*{re.escape(character_name)}.*", re.IGNORECASE)
+    waifu = collection.find_one({'name': regex_pattern})
+    if waifu:
+        return jsonify({
+            'character_name': waifu['name'],
+            'anime_name': waifu['anime'],
+            'image_url': waifu['img_url'],
+            'rarity': waifu.get('rarity', 'Unknown'),
+            'id': waifu.get('id', 'N/A')
+        })
+    else:
+        return jsonify({'error': 'Waifu not found'}), 404
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
