52a8f55afba19a00387cde08c3fac8abc49a0b18
ishu9805 <abhishek47903@gmail.com>
2024-12-01 14:18:58 +0530

    Update auction.py

--- shivu/modules/auction.py
+++ shivu/modules/auction.py
@@ -214,8 +214,7 @@ async def place_bid(client, message):
     # Notify the former highest bidder that they have been outbid
     former_highest_bidder_id = active_auction["highest_bidder"]
     if former_highest_bidder_id:
-        former_highest_bidder = await client.get_users(former_highest_bidder_id)
-        former_highest_bidder_username = former_highest_bidder.username if former_highest_bidder.username else "No Username"
+        former_highest_bidder_username = former_highest_bidder_id.username if former_highest_bidder_id.username else "No Username"
         await message.send_message(
             -1002338924488,
             f"{former_highest_bidder_username} ⚠️ You have been outbid in the auction for {active_auction['waifu_name']} ({active_auction['waifu_anime']}).\n"
