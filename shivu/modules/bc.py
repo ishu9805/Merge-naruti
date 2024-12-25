67d1b2d49301a3d327a849eba3021ea234436bfb
ishu9805 <abhishek47903@gmail.com>
2024-12-25 20:49:48 +0530

    Update bc.py

--- shivu/modules/bc.py
+++ shivu/modules/bc.py
@@ -1,11 +1,11 @@
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler 
 
-from shivu import application, top_global_groups_collection, pm_users, OWNER_ID 
+from shivu import application, top_global_groups_collection, pm_users, OWNER_ID, PARTNER
 
 async def broadcast(update: Update, context: CallbackContext) -> None:
     
-    if update.effective_user.id != 7378476666:
+    if str(update.effective_user.id) not in PARTNER:
         await update.message.reply_text("You are not authorized to use this command.")
         return
 
