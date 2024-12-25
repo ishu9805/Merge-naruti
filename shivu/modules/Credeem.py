1c3939e35b35240ecaa2b7d59fe72324325db0de
ishu9805 <abhishek47903@gmail.com>
2024-12-26 03:52:05 +0530

    Update Credeem.py

--- shivu/modules/Credeem.py
+++ shivu/modules/Credeem.py
@@ -233,7 +233,7 @@ async def daily_code(update, context):
         member = await app.get_chat_member(required_group_id, user_id)
         if member.status in ['left', 'kicked']:
           raise Exception("Not a member")
-   except Exception:
+    except Exception:
         group_link = "https://t.me/blade_x_community"  # Replace with the actual group invite link
         message = (
           "You need to be a member of our exclusive group to use this command.\n"
