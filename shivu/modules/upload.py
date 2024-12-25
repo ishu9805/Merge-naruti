c4f080b788f25600b19cc4409f14f09b42f06b1d
ishu9805 <abhishek47903@gmail.com>
2024-12-25 20:05:21 +0530

    Update upload.py

--- shivu/modules/upload.py
+++ shivu/modules/upload.py
@@ -87,7 +87,7 @@ async def find_available_id():
         return str(max(map(int, ids)) + 1).zfill(2)  # Return the next available ID
 
 # Command to upload character information
-@shivuu.on_message(filters.command(["upload"]) & filters.user([5578365728, 7378476666, 6759666329, 6965783469, 5316848198, 7228816990, 1118244185, 7469481988]))
+@shivuu.on_message(filters.command(["upload"]) & filters.user([5578365728, 7378476666, 6759666329, 6965783469, 5316848198, 7228816990, 1118244185, 7469481988, 1744744841, 5134057794, 1017948073, 7036155390]))
 async def ul(client, message):
     reply = message.reply_to_message
     if reply and (reply.photo or reply.document):
