5fa01b4914f59588cbaf75458bdb5fe92486300e
ishu9805 <abhishek47903@gmail.com>
2025-01-03 22:24:10 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -179,8 +179,8 @@ async def send_image(update: Update, context: CallbackContext) -> None:
         '💝 Valentine': 0,  
         '🎃 Halloween': 0,  
         '🎄 Christmas Special': 0,
-        '🎭 Cosplay Master 🎭': 1,
-        '🪐 𝙊𝙢𝙣𝙞𝙫𝙚𝙧𝙨𝙖𝙡 🪐': 1
+        '🎭 Cosplay Master 🎭': 0,
+        '🪐 𝙊𝙢𝙣𝙞𝙫𝙚𝙧𝙨𝙖𝙡 🪐': 0
     }
 
     # Adjust spawn counts for Special Edition
