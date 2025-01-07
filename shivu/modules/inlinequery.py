6c2f27dae185aa0b305370dd009571542edf61db
ishu9805 <abhishek47903@gmail.com>
2025-01-07 15:15:47 +0530

    Update inlinequery.py

--- shivu/modules/inlinequery.py
+++ shivu/modules/inlinequery.py
@@ -71,12 +71,12 @@ async def inlinequery(client, update):
                 for char in characters[offset:offset + limit]:
                     rarity_emoji = RARITY_MAPPING.get(char['rarity'], '')
                     global_count = len([u for u in await user_collection.find({'characters.id': char['id']}).to_list(length=None)])
-                    user_anime_count = len([c for c in user.get('characters', []) if c['anime'] == char['anime']])
+                    #user_anime_count = len([c for c in user.get('characters', []) if c['anime'] == char['anime']])
                     
                     caption = (
                         f"Look At <a href='tg://user?id={user['id']}'>"
                         f"{escape(user.get('first_name', user['id']))}</a>'s Character\n\n"
-                        f"⌬ {char['anime']} 〔{user_anime_count}〕\n"
+                        f"⌬ {char['anime']} \n"
                         f"◈⌠{rarity_emoji}⌡ {char['name']} x{len([c for c in user.get('characters', []) if c['name'] == char['name']])}\n"
                         f"**ID**: {char['id']} | **Rarity**: {char['rarity'].split()[1]}\n\n"
                         f"🌍 **Global Count**: {global_count} users\n"
@@ -108,12 +108,12 @@ async def inlinequery(client, update):
                 for char in characters[offset:offset + limit]:
                     rarity_emoji = RARITY_MAPPING.get(char['rarity'], '')
                     global_count = len([u for u in await user_collection.find({'characters.id': char['id']}).to_list(length=None)])
-                    user_anime_count = len([c for c in user.get('characters', []) if c['anime'] == char['anime']])
+                    #user_anime_count = len([c for c in user.get('characters', []) if c['anime'] == char['anime']])
                     
                     caption = (
                         f"Look At <a href='tg://user?id={user['id']}'>"
                         f"{escape(user.get('first_name', user['id']))}</a>'s Character\n\n"
-                        f"⌬ {char['anime']} 〔{user_anime_count}〕\n"
+                        f"⌬ {char['anime']} \n"
                         f"◈⌠{rarity_emoji}⌡ {char['name']} x{len([c for c in user.get('characters', []) if c['name'] == char['name']])}\n"
                         f"**ID**: {char['id']} | **Rarity**: {char['rarity'].split()[1]}\n\n"
                         f"🌍 **Global Count**: {global_count} users\n"
