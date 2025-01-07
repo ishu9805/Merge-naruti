a3f69464c294806f7b52ac177db2c04092c82db0
ishu9805 <abhishek47903@gmail.com>
2025-01-07 15:51:09 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -1,49 +1,31 @@
+import asyncio
 import datetime
 import importlib
-import time
 import random
 import re
-import asyncio
-from html import escape 
-import asyncio
-import random
+import time
 from html import escape
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, Bot
-from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filters
-from shivu import app
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
-from telegram import Update
-from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filters
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 from telegram.constants import ReactionEmoji
-import random
-
-
-from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu
-from shivu import application, SUPPORT_CHAT, UPDATE_CHAT, db, ban_collection
-from shivu.modules import ALL_MODULES
-from shivu.modules.coin import add_coins, update_leaderboards
-from shivu.modules.leaderboard import create_indexes
-from shivu import LOGGER
-import datetime
-import importlib
-import time
-import random
-import re
-import asyncio
-from html import escape
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, Bot
 from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filters
-from shivu import app
-from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu
-from shivu import application, SUPPORT_CHAT, UPDATE_CHAT, db, ban_collection
+from shivu import (
+    app,
+    collection,
+    top_global_groups_collection,
+    group_user_totals_collection,
+    user_collection,
+    user_totals_collection,
+    shivuu,
+    application,
+    SUPPORT_CHAT,
+    UPDATE_CHAT,
+    db,
+    ban_collection,
+    LOGGER
+)
 from shivu.modules import ALL_MODULES
 from shivu.modules.coin import add_coins, update_leaderboards
 from shivu.modules.leaderboard import create_indexes
-from shivu import LOGGER
 
 all_characters = []
 
