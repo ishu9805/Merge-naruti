b76eb7dbc5af03f07875f5e6e67742e895934fda
ishu9805 <abhishek47903@gmail.com>
2025-01-08 09:01:17 +0530

    Update harem.py

--- shivu/modules/harem.py
+++ shivu/modules/harem.py
@@ -53,15 +53,18 @@ async def harem(update: Update, context: CallbackContext, page=0) -> None:
 
     characters = sorted(user['characters'], key=lambda x: (x['anime'], x['id']))
     rarity_mode = await get_user_rarity_mode(user_id)
-    count = user_info.get('rarity_counts', {}).get(rarity_mode, 0)
+    
     total_count = user_info.get('ccount', 0)
     count_selected_rarity = user_info.get(f'{rarity_mode}_count', 0)
     
     
     if rarity_mode != 'All':
         characters = [char for char in characters if char.get('rarity') == rarity_mode]
+        count = user_info.get('rarity_counts', {}).get(rarity_mode, 0)
+    else:
+        count = total_count
     
-    total_pages = math.ceil({count} / 20)
+    total_pages = math.ceil(count / 20)
     if page < 0 or page >= total_pages:
         page = 0
    
