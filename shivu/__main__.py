d844af0e64f05e0bc2109b744160d341fcca0663
ishu9805 <abhishek47903@gmail.com>
2025-01-08 16:20:38 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -378,6 +378,12 @@ async def fav(update: Update, context: CallbackContext) -> None:
 
     await update.message.reply_text(f'🌟 {character["name"]} has been added to your favorites!')
 
+
+
+async def show_message_count(update: Update, context: CallbackContext) -> None:
+    chat_id = str(update.effective_chat.id)
+    count = message_counters.get(chat_id, 0)
+    await update.message.reply_text(f"Message count for this group: {count}")
     
 
 def error_handler(update: Update, context: CallbackContext):
@@ -390,7 +396,8 @@ def main() -> None:
     application.add_handler(CommandHandler(["guess"], guess, block=False))
     application.add_handler(CommandHandler("fav", fav, block=False))
     application.add_handler(MessageHandler(filters.ALL, message_counter, block=False))
-
+    application.add_handler(CommandHandler("mecount", show_message_count))
+    
     # Use asyncio.create_task to run the bot in the background
     asyncio.create_task(application.run_polling(drop_pending_updates=True))
     application.add_error_handler(error_handler)
