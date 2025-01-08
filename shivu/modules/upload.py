1626d4e4fb9890ba8d2c822bd2994111fe7822ba
ishu9805 <abhishek47903@gmail.com>
2025-01-08 14:31:46 +0530

    Update upload.py

--- shivu/modules/upload.py
+++ shivu/modules/upload.py
@@ -362,7 +362,7 @@ async def upload_video_character(client, message):
     character = {
         'name': character_name,
         'anime': anime,
-        'rarity': "🎗️ 𝘼𝙣𝙞𝙢𝙖𝙩𝙚𝙙",
+        'rarity': "🎗️ 𝘼𝙈𝙑 𝙀𝙙𝙞𝙩𝙞𝙤𝙣",
         'id': available_id,
         'vid_url': vid_url,
     }
@@ -376,7 +376,7 @@ async def upload_video_character(client, message):
                 f"🎥 **New Character Added** 🎥\n\n"
                 f"Character Name: {character_name}\n"
                 f"Anime Name: {anime}\n"
-                f"Rarity: '🎗️ 𝘼𝙣𝙞𝙢𝙖𝙩𝙚𝙙'\n"
+                f"Rarity: '🎗️ 𝘼𝙈𝙑 𝙀𝙙𝙞𝙩𝙞𝙤𝙣'\n"
                 f"ID: {available_id}\n"
                 f"Added by [{message.from_user.first_name}](tg://user?id={message.from_user.id})"
             ),
