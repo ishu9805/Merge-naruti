5cf8e83787cc7f189d37be5884608ea9e2a81a25
ishu9805 <abhishek47903@gmail.com>
2025-01-14 10:17:14 +0530

    Update upload.py

--- shivu/modules/upload.py
+++ shivu/modules/upload.py
@@ -6,7 +6,7 @@ from telegram.ext import CommandHandler, CallbackContext
 
 from shivu import application, sudo_users, collection, db, CHARA_CHANNEL_ID, SUPPORT_CHAT, OWNER_ID, user_collection
 
-from shivu.modules.coin import shops_collection
+from shivu import shops_collection
 from telegraph import upload_file
 from pyrogram import filters
 from shivu import shivuu, collection
