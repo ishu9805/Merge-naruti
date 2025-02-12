9ec62bf7141737598349ed4ec2b6bc674b63fdee
ishu9805 <abhishek47903@gmail.com>
2025-02-12 06:23:47 +0000

    Update wrappers.py

--- shivu/utils/wrappers.py
+++ shivu/utils/wrappers.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from functools import wraps
-from .capsify import capsify 
-from Grabber import user_collection, db
+
+from shivu import user_collection, db
 
 sudb = db.sudo
 devb = db.dev
