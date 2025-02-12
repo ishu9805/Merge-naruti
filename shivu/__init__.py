3c3ff72967a7293a6a3ebe95d2e7b9f8c178955a
ishu9805 <abhishek47903@gmail.com>
2025-02-12 08:15:13 +0000

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -1,6 +1,6 @@
 import logging  #
 import os
-from pyrogram import Client 
+from pyrogram import Client as PyrogramClient
 from telegram.ext import Application
 from motor.motor_asyncio import AsyncIOMotorClient
 import asyncio
