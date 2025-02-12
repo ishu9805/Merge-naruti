f839b899670f456d13d6cf4d37f194266d59ff3c
ishu9805 <abhishek47903@gmail.com>
2025-02-13 03:44:33 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -1,6 +1,6 @@
 
     # Get this value from my.telegram.org/app
-OWNER_ID = "7378476666"
+OWNER_ID = 7378476666
 PARTNER =  "7378476666", "1744744841"
 sudo_users =  "7378476666", "1744744841"
 GROUP_ID = -1002198664660
