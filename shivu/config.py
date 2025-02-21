8a028e4b1495d2e18a6ba356562d36fd539ca392
ishu9805 <abhishek47903@gmail.com>
2025-02-21 04:37:15 +0000

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,8 +6,8 @@ sudo_users =  "7378476666", "1744744841"
 GROUP_ID = -1002198664660
 LOG_CHANNEL = -1002455650183
     #TOKEN = "7187229883:AAFO_cZf76s6dDupZc-PJ_6la9iFoLxY-tI"
-TOKEN = "7107840748:AAHiKR-lF03Dh-P-GlODsTkYk-I0eBgNKW8"
-mongo_url = "mongodb+srv://babusona:hinatababy@cluster0.t0lfelh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+TOKEN = "7107840748:AAGl6VQIM_XMaV6r1fjtk-eJ1HOEPFqL35o"
+mongo_url = "mongodb+srv://nibbanmisal3302:Gokukhan3303@cluster0.0u22b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 PHOTO_URL = ["https://envs.sh/Eiz.mp4", "https://envs.sh/EiL.mp4", "https://envs.sh/Eic.mp4"]
 SUPPORT_CHAT = "naruto_support_chat"
 UPDATE_CHAT = "blade_x_community"
