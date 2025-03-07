dbc2a6dfd7494d475ddc785f75eb4d48c3a7a899
ishu9805 <abhishek47903@gmail.com>
2025-03-07 15:24:19 +0000

    Update Credeem.py

--- shivu/modules/Credeem.py
+++ shivu/modules/Credeem.py
@@ -6,6 +6,7 @@ from shivu import application, user_collection, PARTNER, ban_collection, collect
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 from telegram.ext import ContextTypes
 from .block import block_dec, temp_block, block_dec_ptb, block_cbq_ptb
+from . import app
 from .lock import command_lock
 # MongoDB Collection for user shops
 user_shops_collection = db["dailyshop"]
