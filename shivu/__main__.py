d7c96046bb36aaa530440dee106aab9587fb96d8
ishu9805 <abhishek47903@gmail.com>
2025-03-16 06:08:21 +0000

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -73,6 +73,7 @@ def escape_markdown(text):
     return re.sub(r'([%s])' % re.escape(escape_chars), r'\\\1', text)
 
 
+@block_dec_ptb
 async def message_counter(update: Update, context: CallbackContext) -> None:
     chat_id = str(update.effective_chat.id)
     user_id = update.effective_user.id
