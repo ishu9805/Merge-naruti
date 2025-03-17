b91d33efd20a6db18eebebf25464bbe04b27e23e
ishu9805 <abhishek47903@gmail.com>
2025-03-17 10:12:08 +0530

    Update Callbackpyro.py

--- shivu/modules/Callbackpyro.py
+++ shivu/modules/Callbackpyro.py
@@ -12,13 +12,15 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pymongo import MongoClient
 from shivu.modules.gift import on_gift_callback_query, on_trade_callback_query
 from shivu.modules.fav import button
+from shivu.modules.pfp import delete_profile_media_callback
 
 # Handler for callback queries
 @app.on_callback_query()
 async def callback_query_handler(client, callback_query):
     try:
         data = callback_query.data
-
+        if data.startswith("delete_pfp"):
+            await delete_profile_media_callback(client, callback_query)
         if data.startswith("sgift") or data.startswith("cgift"):
             await on_gift_callback_query(client, callback_query)
         elif data.startswith("confirm_trade_receiver:") or data.startswith("cancel_trade:"):
