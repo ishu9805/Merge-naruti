79e14857345ce54ebae9399b05d5154a974ca349
ishu9805 <abhishek47903@gmail.com>
2025-03-17 10:29:52 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -194,17 +194,17 @@ async def send_image(update: Update, context: CallbackContext) -> None:
         '💸 Premium Edition': 0,
         '🌤 Summer': 0 if today_message_count <= 4 else 0,
         '🎐 Celestial': 1 if datetime.datetime.today().weekday() in [0, 7] else 0,
-        '❄️ Winter': 1,
-        '🟡 Legendary': 5, 
+        '❄️ Winter': 0,
+        '🟡 Legendary': 0, 
         '⚪️ Common': 5,
          # Stop spawning Winter characters
-        '💝 Valentine': 1, 
-        '⚪️ Common': 5,  # Start spawning Valentine characters
-        '🎃 Halloween': 1,
+        '💝 Valentine': 0, 
+        '⚪️ Common': 0,  # Start spawning Valentine characters
+        '🎃 Halloween': 0,
         '🟡 Legendary': 5, 
         '⚪️ Common': 5,
-        '🎄 Christmas Special': 1,
-        '🎭 Cosplay Master 🎭': 0,
+        '🎄 Christmas Special': 0,
+        '🎭 Cosplay Master 🎭': 1,
         '🪐 𝙊𝙢𝙣𝙞𝙫𝙚𝙧𝙨𝙖𝙡 🪐': 0,
         '🎗️ 𝘼𝙈𝙑 𝙀𝙙𝙞𝙩𝙞𝙤𝙣': 0
     }
