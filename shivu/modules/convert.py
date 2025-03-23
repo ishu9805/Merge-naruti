e61889dda82dcaacc9d32c637a0a96e7d0d652c7
ishu9805 <abhishek47903@gmail.com>
2025-03-23 22:51:54 +0530

    Update convert.py

--- shivu/modules/convert.py
+++ shivu/modules/convert.py
@@ -3,16 +3,35 @@ import importlib
 import random
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filters, Application
-from shivu import collection, user_collection, shivuu
-from . import app
+#from shivu import collection, user_collection, shivuu
+#from . import app
 from .lock import command_lock
-from shivu import application, ban_collection
+#from shivu import application, ban_collection
 
 
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from shivu import user_collection, ban_collection, shivuu as app
+#from shivu import user_collection, ban_collection, shivuu as app
+
+from shivu import UPDATE_CHAT, SUPPORT_CHAT, CHARA_CHANNEL_ID, required_group_id, PHOTO_URL, OWNER_ID, PARTNER
+from shivu import (
+    collectionps as collection,
+    top_global_groups_collectionps as top_global_groups_collection,
+    group_user_totals_collectionps as group_user_totals_collection,
+    user_collectionps as user_collection,
+    user_totals_collectionps as user_totals_collection,
+    shivuups as shivuu,
+    shivuups as app,
+    applicationps as application,
+    SUPPORT_CHATps as SUPPORT,
+    UPDATE_CHATps as UPDATE_CHAT,
+    dbps as db,
+    pmusersps as pmusers,
+    ban_collectionps as ban_collection,
+    user_countps as user_count, 
+    chat_dataps as chat_data,
+)
 
 # Conversion rate
 COIN_TO_TOKEN_RATE = 100  # 100 coins = 1 token
