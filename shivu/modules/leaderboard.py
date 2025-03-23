464aeeb8d3dceee5c961d67975788119bdd4bf84
ishu9805 <abhishek47903@gmail.com>
2025-03-23 22:42:07 +0530

    Update leaderboard.py

--- shivu/modules/leaderboard.py
+++ shivu/modules/leaderboard.py
@@ -5,11 +5,26 @@ import html
 import logging
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext, Application
-from shivu import (application, PHOTO_URL, OWNER_ID, user_collection, 
-                   top_global_groups_collection, group_user_totals_collection, 
-                   sudo_users as SUDO_USERS)
 from cachetools import TTLCache
-from shivu import PARTNER, ban_collection
+from shivu import PARTNER
+from shivu import UPDATE_CHAT, SUPPORT_CHAT, CHARA_CHANNEL_ID, required_group_id, PHOTO_URL, OWNER_ID, PARTNER
+from shivu import (
+    collectionps as collection,
+    top_global_groups_collectionps as top_global_groups_collection,
+    group_user_totals_collectionps as group_user_totals_collection,
+    user_collectionps as user_collection,
+    user_totals_collectionps as user_totals_collection,
+    shivuups as shivuu,
+    shivuups as app,
+    applicationps as application,
+    SUPPORT_CHATps as SUPPORT,
+    UPDATE_CHATps as UPDATE_CHAT,
+    dbps as db,
+    pmusersps as pmusers,
+    ban_collectionps as ban_collection,
+    user_countps as user_count, 
+    chat_dataps as chat_data,
+)
 
 
 logging.basicConfig(level=logging.INFO)
