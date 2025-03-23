96ca3855b6b143ff5c450444f248b65f15fc4543
ishu9805 <abhishek47903@gmail.com>
2025-03-23 22:22:58 +0530

    Update Start.py

--- shivu/modules/Start.py
+++ shivu/modules/Start.py
@@ -1,10 +1,28 @@
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
 import random
-from . import user_collection, app
+#from . import user_collection, app
 from shivu import *
 from .block import block_dec, temp_block, block_cbq
 from datetime import datetime
+from shivu import UPDATE_CHAT, SUPPORT_CHAT, CHARA_CHANNEL_ID, required_group_id, PHOTO_URL, OWNER_ID, PARTNER
+from shivu import (
+    collectionps as collection,
+    top_global_groups_collectionps as top_global_groups_collection,
+    group_user_totals_collectionps as group_user_totals_collection,
+    user_collectionps as user_collection,
+    user_totals_collectionps as user_totals_collection,
+    shivuups as shivuu,
+    shivuups as app,
+    applicationps as application,
+    SUPPORT_CHATps as SUPPORT,
+    UPDATE_CHATps as UPDATE_CHAT,
+    dbps as db,
+    pmusersps as pmusers,
+    ban_collectionps as ban_collection,
+    user_countps as user_count, 
+    chat_dataps as chat_data,
+)
 
 sudb = db.sudo
 devb = db.dev
