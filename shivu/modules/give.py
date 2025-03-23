a5423e297f3aab1d87c57e918f12ab89ff1c6a1a
ishu9805 <abhishek47903@gmail.com>
2025-03-23 22:44:32 +0530

    Update give.py

--- shivu/modules/give.py
+++ shivu/modules/give.py
@@ -6,8 +6,24 @@ import random
 
 from telegram.ext import CommandHandler, CallbackContext, CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
-from shivu import collection, user_collection, application, PARTNER, ban_collection, user_count
+from shivu import UPDATE_CHAT, SUPPORT_CHAT, CHARA_CHANNEL_ID, required_group_id, PHOTO_URL, OWNER_ID, PARTNER
+from shivu import (
+    collectionps as collection,
+    top_global_groups_collectionps as top_global_groups_collection,
+    group_user_totals_collectionps as group_user_totals_collection,
+    user_collectionps as user_collection,
+    user_totals_collectionps as user_totals_collection,
+    shivuups as shivuu,
+    shivuups as app,
+    applicationps as application,
+    SUPPORT_CHATps as SUPPORT,
+    UPDATE_CHATps as UPDATE_CHAT,
+    dbps as db,
+    pmusersps as pmusers,
+    ban_collectionps as ban_collection,
+    user_countps as user_count, 
+    chat_dataps as chat_data,
+)
 
 # Lock to ensure thread-safe access to the global variable
 async def rarities(update: Update, context: CallbackContext):
