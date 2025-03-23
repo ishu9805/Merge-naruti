7238f1ab850f35491dba07da6367cff34a3654e2
ishu9805 <abhishek47903@gmail.com>
2025-03-23 22:10:32 +0530

    Update Inlinequery.py

--- shivu/modules/Inlinequery.py
+++ shivu/modules/Inlinequery.py
@@ -6,8 +6,24 @@ from pymongo import MongoClient, ASCENDING, DESCENDING
 from pyrogram import Client, filters
 from pyrogram.types import InlineQueryResultPhoto, InlineQueryResultVideo
 
-from shivu import user_collection, collection, db
-from shivu import shivuu as app
+from shivu import UPDATE_CHAT, SUPPORT_CHAT, CHARA_CHANNEL_ID, required_group_id, PHOTO_URL, OWNER_ID, PARTNER
+from shivu import (
+    collectionps as collection,
+    top_global_groups_collectionps as top_global_groups_collection,
+    group_user_totals_collectionps as group_user_totals_collection,
+    user_collectionps as user_collection,
+    user_totals_collectionps as user_totals_collection,
+    shivuups as shivuu,
+    shivuups as app,
+    applicationps as application,
+    SUPPORT_CHATps as SUPPORT,
+    UPDATE_CHATps as UPDATE_CHAT,
+    dbps as db,
+    pmusersps as pmusers,
+    ban_collectionps as ban_collection,
+    user_countps as user_count, 
+    chat_dataps as chat_data,
+)
 
 # Database Indexing
 db.characters.create_index([('id', ASCENDING)])
