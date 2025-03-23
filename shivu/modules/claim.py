2bde84de574ac7bf25467115b78bea1c27c2bb24
ishu9805 <abhishek47903@gmail.com>
2025-03-23 22:55:27 +0530

    Update claim.py

--- shivu/modules/claim.py
+++ shivu/modules/claim.py
@@ -4,9 +4,25 @@ import logging
 from datetime import datetime, timedelta
 from pyrogram import Client, filters, types as t
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from shivu import shivuu as bot, user_collection, collection, ban_collection, PARTNER, required_group_id 
-from shivu import application, user_count
-# Constants
+from shivu import UPDATE_CHAT, SUPPORT_CHAT, CHARA_CHANNEL_ID, required_group_id, PHOTO_URL, OWNER_ID, PARTNER
+from shivu import (
+    collectionps as collection,
+    top_global_groups_collectionps as top_global_groups_collection,
+    group_user_totals_collectionps as group_user_totals_collection,
+    user_collectionps as user_collection,
+    user_totals_collectionps as user_totals_collection,
+    shivuups as shivuu,
+    shivuups as app,
+    applicationps as application,
+    SUPPORT_CHATps as SUPPORT,
+    UPDATE_CHATps as UPDATE_CHAT,
+    dbps as db,
+    pmusersps as pmusers,
+    ban_collectionps as ban_collection,
+    user_countps as user_count, 
+    chat_dataps as chat_data,
+)
+
 DEVS = (7378476666)
 CHAT_ID = "-1002338924488"
 JOIN_URL = "https://t.me/naruto_support_chat"
