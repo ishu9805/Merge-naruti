cf653626e1f7efa32b6abd1cf74539a14b5f1f43
ishu9805 <abhishek47903@gmail.com>
2025-03-23 21:20:13 +0530

    Update Config.py

--- moto/Config.py
+++ moto/Config.py
@@ -1,1 +1,23 @@
 
+    # Get this value from my.telegram.org/app
+OWNER_ID = 7378476666
+PARTNER =  "7378476666", "1744744841"
+sudo_users =  "7378476666", "1744744841"
+GROUP_ID = -1002198664660
+LOG_CHANNEL = -1002455650183
+#TOKEN = "7187229883:AAFO_cZf76s6dDupZc-PJ_6la9iFoLxY-t"
+TOKENsingings = "7107840748:AAFc8v9lLpHPafEplEXLcoOmNSiiqe-O2XE"
+mongo_urlings = "mongodb+srv://nibbanmisal3302:Gokukhan3303@cluster0.0u22b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+PHOTO_URL = ["https://envs.sh/Eiz.mp4", "https://envs.sh/EiL.mp4", "https://envs.sh/Eic.mp4"]
+SUPPORT_CHAT = "naruto_support_chat"
+UPDATE_CHAT = "blade_x_community"
+BOT_USERNAME = "Fancy_Waifu_Husbando_Bot"
+CHARA_CHANNEL_ID = "-1002117539029"
+api_id = 22792918
+api_hash = "ff10095d2bb96d43d6eb7a7d9fc85f81"
+required_group_id = -1001999201034
+STRICT_GBAN = True
+ALLOW_CHATS = True
+ALLOW_EXCL = True
+DEL_CMDS = True
+INFOPIC = True
