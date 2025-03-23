95f2bff6646857e1f2a29ed9037f40d2c744d30a
ishu9805 <abhishek47903@gmail.com>
2025-03-23 22:29:40 +0530

    Update top.py

--- shivu/modules/top.py
+++ shivu/modules/top.py
@@ -5,16 +5,29 @@ import html
 import logging
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext, Application
-from shivu import (
-    application, PHOTO_URL, OWNER_ID, user_collection, 
-    top_global_groups_collection, group_user_totals_collection, 
-    sudo_users as SUDO_USERS, PARTNER, ban_collection
-)
+
 
 from cachetools import TTLCache
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from datetime import datetime
-
+from shivu import UPDATE_CHAT, SUPPORT_CHAT, CHARA_CHANNEL_ID, required_group_id, PHOTO_URL, OWNER_ID, PARTNER
+from shivu import (
+    collectionps as collection,
+    top_global_groups_collectionps as top_global_groups_collection,
+    group_user_totals_collectionps as group_user_totals_collection,
+    user_collectionps as user_collection,
+    user_totals_collectionps as user_totals_collection,
+    shivuups as shivuu,
+    shivuups as app,
+    applicationps as application,
+    SUPPORT_CHATps as SUPPORT,
+    UPDATE_CHATps as UPDATE_CHAT,
+    dbps as db,
+    pmusersps as pmusers,
+    ban_collectionps as ban_collection,
+    user_countps as user_count, 
+    chat_dataps as chat_data,
+)
 # Logging setup
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
