c84097bb5858354e74982cfbcb487821a392b3c0
ishu9805 <abhishek47903@gmail.com>
2025-04-12 06:30:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -57,16 +57,23 @@ def search_waifus():
         query_filters['id'] = {'$regex': id_query, '$options': 'i'}
 
     waifus = list(collection.find(query_filters))
-    results = []
-    for waifu in waifus:
-        media_url = waifu.get('vid_url') or waifu.get('img_url')
-        results.append({
-            'character_name': waifu['name'],
-            'anime_name': waifu['anime'],
-            'media_url': media_url,
-            'rarity': waifu.get('rarity', 'Unknown'),
-            'id': waifu.get('id', 'N/A')
-        })
+    results = [{
+        'character_name': waifu['name'],
+
+
+        'anime_name': waifu['anime'],
+
+
+        'image_url': waifu['img_url'],
+
+
+        'rarity': waifu.get('rarity', 'Unknown'),
+
+
+        'id': waifu.get('id', 'N/A')
+
+
+    } for waifu in waifus]
 
     return jsonify({'results': results})
 
