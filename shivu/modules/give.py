848523a839839935bfd85e49fe9e311b2abf3f75
ishu9805 <abhishek47903@gmail.com>
2025-04-17 18:21:09 +0530

    Update give.py

--- shivu/modules/give.py
+++ shivu/modules/give.py
@@ -55,7 +55,8 @@ async def rarities(update: Update, context: CallbackContext):
         "🪐 𝙊𝙢𝙣𝙞𝙫𝙚𝙧𝙨𝙖𝙡 🪐": 0,
         "🎭 Cosplay Master 🎭": 0,
         "🎗️ 𝘼𝙈𝙑 𝙀𝙙𝙞𝙩𝙞𝙤𝙣": 0,
-        "🧧 𝙀𝙫𝙚𝙣𝙩𝙨": 0
+        "🧧 𝙀𝙫𝙚𝙣𝙩𝙨": 0,
+        "🍑 Echhi":0
     }
 
     async for character in characters_cursor:  # Iterate over the cursor asynchronously
