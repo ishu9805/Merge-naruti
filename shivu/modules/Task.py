67465b9e32e0347301eced0561d757bf0adf85fc
ishu9805 <abhishek47903@gmail.com>
2025-04-28 19:05:57 +0530

    Update Task.py

--- shivu/modules/Task.py
+++ shivu/modules/Task.py
@@ -385,6 +385,8 @@ async def reset_task_command(client, message):
         reply_markup=confirm_buttons
     )
 
+
+@cmd
 @app.on_callback_query(filters.regex(r"^confirm_reset_(\d+)$"))
 async def confirm_reset(client, callback_query):
     user_id = int(callback_query.matches[0].group(1))
