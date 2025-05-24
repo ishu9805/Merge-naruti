420721d6ae191831a6dbb219aeb4d0cfd1255a75
ishu9805 <abhishek47903@gmail.com>
2025-05-24 10:23:57 +0530

    Update Callbackpyro.py

--- shivu/modules/Callbackpyro.py
+++ shivu/modules/Callbackpyro.py
@@ -9,7 +9,7 @@ from pymongo import MongoClient
 from shivu.modules.gift import on_gift_callback_query, on_trade_callback_query
 from shivu.modules.fav import button
 from shivu.modules.Animelist import anime_letter_callback, anime_selection_callback, back_to_az_callback
-from shivu.modules.Task import confirm_claim, cancel_claim, confirm_reset, cancel_reset
+from shivu.modules.Task import confirm_claim, cancel_claim # confirm_reset, cancel_reset
 from shivu.modules.Pfp import approve_profile_media, reject_profile_media
 from shivu import UPDATE_CHAT, SUPPORT_CHAT, CHARA_CHANNEL_ID, required_group_id, PHOTO_URL, OWNER_ID, PARTNER
 from shivu import (
