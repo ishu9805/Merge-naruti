82e903421683521749a14b1660c82bee1899b9ea
ishu9805 <abhishek47903@gmail.com>
2025-05-25 05:13:28 +0000

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -46,7 +46,7 @@ reaction_list = [ReactionEmoji.THUMBS_UP, ReactionEmoji.EYES, ReactionEmoji.CLAP
 current_amv_character = {}  # Tracks AMV characters per chat
 amv_claim_limit = 1  #
 
-AMV_GROUP_ID = "-1002606804832" # Your main group ID
+AMV_GROUP_ID = -1002606804832 # Your main group ID
  # Spawn every 100 messages
 MAX_AMV_OWNERS = 10  # Global ownership limit
 amv_spawn_counter = 0  # Track message count for AMV spawns
