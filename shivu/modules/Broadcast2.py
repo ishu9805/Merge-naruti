6e0f416969d63502ba47665288b2d5fd3a347c4b
ishu9805 <abhishek47903@gmail.com>
2025-05-26 22:28:43 +0530

    Update Broadcast2.py

--- shivu/modules/Broadcast2.py
+++ shivu/modules/Broadcast2.py
@@ -21,7 +21,7 @@ from shivu import (
 )
 async def gbroadcast(update: Update, context: CallbackContext) -> None:
     
-    if update.effective_user.id != 7378476666:
+    if update.effective_user.id != 741099256:
         await update.message.reply_text("You are not authorized to use this command.")
         return
 
@@ -49,5 +49,37 @@ async def gbroadcast(update: Update, context: CallbackContext) -> None:
 
     await update.message.reply_text(f"Broadcast complete. Failed to send to {failed_sends} chats/users.")
 
-application.add_handler(CommandHandler("cgbroadcast", gbroadcast, block=False))
+application.add_handler(CommandHandler("ggbroadcast", gbroadcast, block=False))
 
+
+async def ubroadcast(update: Update, context: CallbackContext) -> None:
+    
+    if update.effective_user.id != 741099256:
+        await update.message.reply_text("You are not authorized to use this command.")
+        return
+
+    message_to_broadcast = update.message.reply_to_message
+
+    if message_to_broadcast is None:
+        await update.message.reply_text("Please reply to a message to broadcast.")
+        return
+
+    all_chats = await user_collection.distinct("id")
+    #  all_users = await pm_users.distinct("_id")
+
+    shuyaa = list(set(all_chats))
+
+    failed_sends = 0
+
+    for chat_id in shuyaa:
+        try:
+            await context.bot.forward_message(chat_id=chat_id,
+                                              from_chat_id=message_to_broadcast.chat_id,
+                                              message_id=message_to_broadcast.message_id)
+        except Exception as e:
+            print(f"Failed to send message to {chat_id}: {e}")
+            failed_sends += 1
+
+    await update.message.reply_text(f"Broadcast complete. Failed to send to {failed_sends} chats/users.")
+
+application.add_handler(CommandHandler("ugbroadcast", ubroadcast, block=False))
