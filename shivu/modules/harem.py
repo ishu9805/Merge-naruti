e676f10712e0ed6e6c59e1c7f31e99bbb8366b05
ishu9805 <abhishek47903@gmail.com>
2025-06-17 05:06:55 +0000

    Update harem.py

--- shivu/modules/harem.py
+++ shivu/modules/harem.py
@@ -57,8 +57,8 @@ RARITY_MAPPING = {
 }
 
 
-@ptbfj
-@must_dm
+@ptbfj()
+
 async def harem(update: Update, context: CallbackContext, page=0) -> None:
     user_id = update.effective_user.id
     user = await user_collection.find_one({'id': user_id})
