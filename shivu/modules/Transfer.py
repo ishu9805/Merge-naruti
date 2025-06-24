ea190767c14257bcba36181045c4d2d1cc41a0e9
ishu9805 <abhishek47903@gmail.com>
2025-06-24 10:52:41 +0530

    Update Transfer.py

--- shivu/modules/Transfer.py
+++ shivu/modules/Transfer.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 
 from shivu import user_collectionps as user_collection, shivuups as shivuu
 
-AUTHORIZED_USER_ID = 7378476666
+AUTHORIZED_USER_ID = 6902029663
 
 @shivuu.on_message(filters.command("ntransfer"))
 async def transfer(client, message):
