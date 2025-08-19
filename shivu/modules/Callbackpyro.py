bc7299316a8bcd1d95bbcb5afc4265f3b735b648
ishu9805 <abhishek47903@gmail.com>
2025-08-19 09:26:40 +0530

    Update Callbackpyro.py

--- shivu/modules/Callbackpyro.py
+++ shivu/modules/Callbackpyro.py
@@ -88,7 +88,8 @@ async def callback_query_handler(client, callback_query):
                 ],
                 [
                     InlineKeyboardButton("🎗️", callback_data="rarity:🎗️ 𝘼𝙈𝙑 𝙀𝙙𝙞𝙩𝙞𝙤𝙣"),
-                    InlineKeyboardButton("🧧", callback_data="rarity:🧧 𝙀𝙫𝙚𝙣𝙩𝙨")
+                    InlineKeyboardButton("🧧", callback_data="rarity:🧧 𝙀𝙫𝙚𝙣𝙩𝙨"),
+                    InlineKeyboardButton("☔", callback_data="rarity: ☔ Monsoon")
                 ]
             ]
 
