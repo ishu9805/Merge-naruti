703c82c9781209c9b24e2954f3765944ae36653a
ishu9805 <abhishek47903@gmail.com>
2025-09-04 20:28:39 +0530

    Update claim.py

--- shivu/modules/claim.py
+++ shivu/modules/claim.py
@@ -67,7 +67,7 @@ async def hclaim(_, message: t.Message):
     mention = message.from_user.mention
 
 
-    if str(message.chat.id) != -1002783891820":
+    if str(message.chat.id) != "-1002783891820":
         await message.reply_text("you can only use this command here @hclaim_support")
         return
         
