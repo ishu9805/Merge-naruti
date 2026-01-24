a0a891db8b6771e5b42c0ea3a602d551c223ce0a
ishu9805 <abhishek47903@gmail.com>
2026-01-24 17:58:07 +0530

    Create app.py

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from routes.search import search_bp
+
+app = Flask(
+    __name__,
+    template_folder="../frontend/templates",
+    static_folder="../frontend/static"
+)
+
+app.register_blueprint(search_bp)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
