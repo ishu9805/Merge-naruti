fc4f57fa8f2adda49601644c20f78c57566e2caf
ishu9805 <abhishek47903@gmail.com>
2026-02-14 08:16:04 +0530

    Fix Sendall export for _send_media_to_channel

--- shivu/modules/Sendall.py
+++ shivu/modules/Sendall.py
@@ -1,1 +1,4 @@
-from shivu.modules.admin.Sendall import *
+"""Compatibility exports for sendall helpers."""
+
+from shivu.modules.admin.Sendall import *  # noqa: F401,F403
+from shivu.modules.admin.Sendall import _send_media_to_channel  # noqa: F401
